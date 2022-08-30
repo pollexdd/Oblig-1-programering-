f3242253832a16da943ef2082117a5f4b3235996
pollekillerxd <76852143+pollekillerxd@users.noreply.github.com>
2022-08-30 20:20:12 +0200

    Update Oppgave 3 - Operatorer.py

--- Oppgave 3 - Operatorer.py
+++ Oppgave 3 - Operatorer.py
@@ -1,7 +1,6 @@
 # Jeg fikk veldig mye hjelp på denne oppgaven av en youtube video får å skjønne hvordan det skulle gjøres
 # link : https://www.youtube.com/watch?v=5_CAo_C523g
 
-
 print("1 får å gange")
 print("2 får å dele")
 print("3 får pluss")
@@ -10,10 +9,10 @@ print("5 får modulo")
 print("6 får opphøyne")
 print("7 får dele med nedrunding")
 
-valg = input("velg modus ")
+valg = input("velg modus: ")
 
-nummer1 = float(input("skriv tall 1 "))
-nummer2 = float(input("skriv tall 2 "))
+nummer1 = float(input("skriv tall 1: "))
+nummer2 = float(input("skriv tall 2: "))
 
 if valg == "1":
     print(nummer1*nummer2)
