bb31a04befd797a73456cfb43ea51b74187b4d8f
pollekillerxd <76852143+pollekillerxd@users.noreply.github.com>
2022-08-30 19:34:19 +0200

    Create Oppgave 3 - Operatorer.py

---
+++ Oppgave 3 - Operatorer.py
@@ -0,0 +1,17 @@
+# Jeg fikk veldig mye hjelp på denne oppgaven av en youtube video får å skjønne hvordan det skulle gjøres
+# link : https://www.youtube.com/watch?v=5_CAo_C523g
+
+print("1 gange")
+print("2 dele")
+print("3 pluss")
+print("4 minus")
+print("5 modulo")
+print("6 opphøyne")
+print("7 dele med nedrunding")
+
+valg = input("velg modus ")
+
+nummer1 = float(input("skriv tall 1 "))
+nummer2 = float(input("skriv tall 2 "))
+
+if valg
